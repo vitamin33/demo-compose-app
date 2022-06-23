1b27475d5b5ec838428b286e2d9bd0d909eda045
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-23 11:46:26 +0300

    Add list with projects

--- app/src/main/java/com/serbyn/demo_compose_app/MainActivity.kt
+++ app/src/main/java/com/serbyn/demo_compose_app/MainActivity.kt
@@ -8,16 +8,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.serbyn.demo_compose_app.ui.theme.DemocomposeappTheme
@@ -40,8 +47,12 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Preview
 @Composable
 fun CreateBizCard() {
+    val portfolioClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -58,7 +69,7 @@ fun CreateBizCard() {
         ) {
             Column(
                 modifier = Modifier
-                    .height(300.dp)
+                    .fillMaxHeight()
                     .fillMaxWidth(),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -67,10 +78,72 @@ fun CreateBizCard() {
                 Divider(color = Color.LightGray, thickness = 0.2.dp)
                 CreateInfo()
                 Button(onClick = {
+                    portfolioClickedState.value = !portfolioClickedState.value
                     Log.d("MainActivity", "button clicked")
                 }) {
-                    Text(text = "Portfolio",
-                    style = MaterialTheme.typography.labelLarge)
+                    Text(
+                        text = "Portfolio",
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+                if (portfolioClickedState.value) {
+                    Content()
+                } else {
+                    Box() {}
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth()
+                    .background(Color.Blue),
+                shape = RectangleShape
+            ) {
+                Row(modifier = Modifier.padding(8.dp)) {
+                    CreateProfileImage(modifier = Modifier.size(50.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .align(
+                                alignment = Alignment.CenterVertically
+                            )
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(
+                            text = "A great project",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
                 }
             }
         }
@@ -104,9 +177,9 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -121,7 +194,7 @@ private fun CreateProfileImage() {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DemocomposeappTheme {
